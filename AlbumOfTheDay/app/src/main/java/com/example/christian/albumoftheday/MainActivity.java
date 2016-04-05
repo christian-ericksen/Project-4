035206ebd99be4b946cf5367b32593535ca1910d
christian-ericksen <christian.ericksen@gmail.com>
2016-04-05 05:09:39 -0400

    third commit

--- AlbumOfTheDay/app/src/main/java/com/example/christian/albumoftheday/MainActivity.java
+++ AlbumOfTheDay/app/src/main/java/com/example/christian/albumoftheday/MainActivity.java
@@ -148,7 +148,7 @@ public class MainActivity extends Activity implements
             comeAsyncTask.execute();
             juicyAsyncTask.execute();
         } else {
-            stopService(i);
+            //stopService(i);
             mPlayer.pause();
         }
         mIsPlaying = !mIsPlaying;
